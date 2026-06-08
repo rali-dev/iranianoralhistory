336d666792ab892b08164ddcf8e2029bc4ff1025
AliRahimiAlfenory <ali.saee@outlook.de>
2026-06-08 17:32:41 +0200

    add backend unit test config and fix frontend unit tests

---
+++ apps/backend/jest.config.cts
@@ -0,0 +1,5 @@
+module.exports = {
+  displayName: 'backend',
+  preset: '../../jest.preset.js',
+  coverageDirectory: '../../coverage/apps/backend',
+};
--- apps/frontend/src/app/app.spec.ts
+++ apps/frontend/src/app/app.spec.ts
@@ -1,19 +1,38 @@
 import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
   });
 
-  it('should render title', async () => {
+  afterEach(() => {
+    TestBed.inject(HttpTestingController).verify();
+  });
+
+  it('should render title', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
+    const httpTesting = TestBed.inject(HttpTestingController);
+
+    fixture.detectChanges();
+    httpTesting
+      .expectOne('/api')
+      .flush({ id: '1', name: 'John Doe', email: 'john@example.com' });
+    fixture.detectChanges();
+
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('frontend');
   });
