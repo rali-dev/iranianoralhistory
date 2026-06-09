4c4be808e7c12488211f8394a824ce3c203b5e9d
AliRahimiAlfenory <ali.saee@outlook.de>
2026-06-10 01:26:34 +0200

    13. set up JWT authentication layer and protect user routes
    - Installed passport, passport-jwt, and their respective types
    - Imported PassportModule into auth.module.ts
    - Created jwt.guard.ts and jwt.strategy.ts for token validation
    - Imported JwtStrategy into user.module.ts
    - Integrated JwtAuthGuard into user.controller.ts to protect endpoints

--- libs/backend/auth/src/lib/auth.module.ts
+++ libs/backend/auth/src/lib/auth.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 @Module({
-  imports: [JwtModule],
+  imports: [JwtModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [AuthService],
 })
 export class AuthModule {}
