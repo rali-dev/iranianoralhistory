d39a26937aadd41f2b76393e21721345e0a29ee0
AliRahimiAlfenory <ali.saee@outlook.de>
2026-06-09 18:32:57 +0200

    10. JWT & Password Hashing Infrastructure
    - Added `JWT_SECRET` variable to the `.env` configuration file for secure token signing
    - Installed required dependencies: `bcrypt`, `@types/bcrypt` for secure password hashing, and `@nestjs/jwt` for token handling
    - Updated the central entry point (`index.ts`) in the auth library
    - Integrated and configured the `JwtModule`
    - Implemented authentication functionalities (SignIn, SignUp, and SignOut) inside `auth.service.ts` using bcrypt hashing and JWT generation
    - Created a `constants.ts` file inside the auth library

--- libs/backend/auth/src/index.ts
+++ libs/backend/auth/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/auth.module';
+export * from './lib/auth.service';
+export * from './lib/auth.controller';
+export * from './lib/config/constants';
--- libs/backend/auth/src/lib/auth.service.ts
+++ libs/backend/auth/src/lib/auth.service.ts
@@ -1,19 +1,71 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@iranianoralhistory/database';
 import { AuthDto } from '@iranianoralhistory/validation';
-
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { jwtSecret } from './config/constants';
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService, private readonly jwtService: JwtService) {}
+
   async signup(dto: AuthDto) {
     const { email, password } = dto;
+    const foundUser = await this.prisma.user.findUnique({ where: { email } });
+    if (foundUser) {
+      throw new BadRequestException('Email is already in use');
+    }
+    const hashedPassword = await this.hashPassword(password);
+    await this.prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+      },
+    });
     return { message: 'signup was successful' };
   }
+  
   async signin(dto: AuthDto) {
     const { email, password } = dto;
-    return { message: 'signin was successful' };
+    const foundUser = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!foundUser) {
+      throw new BadRequestException('Wrong credentials');
+    }
+    const isMatch = await this.comparePasswords({ 
+      password,
+      hashedPassword: foundUser.hashedPassword 
+    });
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Wrong credentials');
+    }
+
+    const token = await this.signToken({ 
+      id: foundUser.id, 
+      email: foundUser.email 
+    });
+    return { message: 'signin was successful', token };
   }
+
   async signout() {
     return { message: 'signout was successful' };
+  }
+
+  async hashPassword(password: string){
+    const saltOrRounds = 10;
+    return  await bcrypt.hash(password, saltOrRounds);
+  }
+
+  async comparePasswords(args: { password: string; hashedPassword: string }) {
+    return await bcrypt.compare(args.password, args.hashedPassword);
+  }  
+
+  async signToken(args: { id: string; email: string }) {
+    const payload = args;
+    return await this.jwtService.signAsync(
+      payload, 
+      { secret: jwtSecret },
+    );
   }
 }
